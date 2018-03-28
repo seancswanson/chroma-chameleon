156b7c17a4ac6ee6b29dcca7a600b001838ce8da
Sean Swanson <sean-swan@hotmail.com>
2018-03-27 19:01:21 -0700

    Added Logic for changing size, and canvas size, now working on tool selection

--- js/script.js
+++ js/script.js
@@ -1,16 +1,76 @@
+// Create Canvas & Context
 const canvas = document.querySelector("#main-canvas");
-const colorPicker = document.querySelector("#input--colorpicker");
-const sizePicker = document.querySelector("#input--sizepicker");
 const c = canvas.getContext("2d");
+// Settings Selectors
+const tools = document.getElementsByClassName("tool");
+const clearButton = document.querySelector("#input--settings__clear");
+const colorPicker = document.querySelector("#input--colorpicker");
+const sizePickerSlider = document.querySelector("#input--sizepickerslider");
+const sizePickerNumber = document.querySelector("#input--sizepickernumber");
+const canvasWidthInput = document.querySelector("#input--width");
+const canvasHeightInput = document.querySelector("#input--height");
+// Tool Selectors
+tools.forEach(tool => {
+  this.addEventListener("click", changeTool);
+});
+const paintBrush = document.querySelector("#li--paintbrush");
+const eraserTool = document.querySelector("#li--eraser");
+const paintBucket = document.querySelector("#li--paintBucket");
+const textTool = document.querySelector("#li--text");
+const square = document.querySelector("#li--square");
+const circle = document.querySelector("#li--circle");
+
+var MMNav = {
+  init: function() {
+    var nav = document.getElementById("mainNav");
+    nav.onclick = this.onclick;
+  },
+  onclick: function(e) {
+    if (e.target.className == "bundle") {
+      e.target.href = e.target.href + "?name=value";
+    }
+    return true;
+  }
+};
+
+// Initial Value settings
+let canvasWidth = window.innerWidth < 600 ? window.innerWidth - 100 : 600;
+let canvasHeight = window.innerHeight < 550 ? window.innerHeight - 100 : 550;
 let currentStrokeColor = colorPicker.value;
-let currentBrushSize = sizePicker.value;
+let currentBrushSize = sizePickerSlider.value;
 let mouseInitial = false;
 
-// Resize Canvas
+// Canvas & Settings Initializer
+settingInit = () => {
+  canvasWidthInput.value = canvasWidth;
+  canvasHeightInput.value = canvasHeight;
+};
 
-//Draw Objects
+canvasInit = () => {
+  canvas.width = `${canvasWidth}`;
+  canvas.height = `${canvasHeight}`;
+  canvas.onmousedown = drawLine;
+  canvas.onmouseup = drawLine;
+};
 
-// Draw 10 pixels on click
+// TODO Resize Canvas
+setWidth = e => {
+  if (e >= 1 && e <= 600) {
+    canvasWidth = e;
+    canvas.width = `${canvasWidth}`;
+  } else {
+    alert("Please enter a width between 1 & 600 pixels");
+  }
+};
+
+setHeight = e => {
+  if (e >= 1 && e <= 550) {
+    canvasHeight = e;
+    canvas.height = `${canvasHeight}`;
+  } else {
+    alert("Please enter a height between 1 & 550 pixels");
+  }
+};
 
 // Change Color
 changeStrokeColor = e => {
@@ -20,32 +80,50 @@ changeStrokeColor = e => {
 // Change Size
 changeLineWidth = e => {
   currentBrushSize = e;
+  sizePickerNumber.value = e;
+  sizePickerSlider.value = e;
 };
+
+// TODO Draw Objects
+
+// TODO Draw 10 pixels on click
+
 // Determine if mouse is pressed
 mouseIsPressed = () => {
   if (mouseInitial === false) {
     mouseInitial = true;
+    executeTool();
   } else {
     mouseInitial = false;
   }
   return;
 };
 
+// Draw a Line between points
 drawLine = () => {
   mouseIsPressed();
   if (mouseIsPressed) {
     c.beginPath();
     c.moveTo(20, 20);
     c.lineTo(20, 100);
-    c.lineTo(70, 100);
     c.strokeStyle = `${currentStrokeColor}`;
     c.lineWidth = `${currentBrushSize}`;
     c.stroke();
   }
 };
 
-canvas.onmousedown = drawLine;
-canvas.onmouseup = drawLine;
+// Clear Canvas
+clearButton.addEventListener("click", function() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+});
 
-console.log(canvas);
-console.log(c);
+executeTool = () => {
+  console.log("Tool is active");
+};
+
+appInit = () => {
+  canvasInit();
+  settingInit();
+};
+
+appInit();
